c18bfaae1e717760d1b296e83bb9adffe3da5719
Hikanyan <hikanyan0131@gmail.com>
2022-08-02 23:17:45 +0900

    プロジェクト ファイルを追加します。

---
+++ d7_pencil/Program.cs
@@ -0,0 +1,12 @@
+﻿// https://paiza.jp/works/mondai/drankfast/d7_pencil
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var line = Console.ReadLine();
+        Console.WriteLine("XXXXXX");
+    }
+}
